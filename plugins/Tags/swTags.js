06f677678137b92e137deda1acf82c60b1a791a5
mamod <mamod.mehyar@gmail.com>
2015-04-10 23:35:03 +0300

    adding tags plugin

---
+++ plugins/Tags/swTags.js
@@ -0,0 +1,73 @@
+(function(){
+    "use strict";
+    
+    //inject style
+    var style = [
+        '<style id="tagitStyle">',
+            '._input_tagit { border: none; border-color: transparent; padding:5px;margin-top:4px;}',
+            '._ul_tagit {list-style:none;padding:0;margin:0;}',
+            '._li_tagit {float:left; display:block;padding:5px;;margin-right:2px;}',
+            '._text_tagit {display:block;float:left;background:#ccc;padding:5px;}',
+            '._icon_tagit {display:block;float:left;background:#ddd;padding:5px;color:#fff;cursor:pointer;}',
+            '',
+        '</style>'
+    ];
+    $(document).find('head').prepend(style.join(''));
+
+    sw.registerBinding('tags', {
+        init : function(){
+            var self = this;
+            var obj = self.valueAccess();
+
+            var tagsObject = {
+                items : obj.items,
+                itemToAdd : sw.observe(""),
+                deleteTag : function(item){
+                    this.node.parent().fadeOut(250, function(){
+                        tagsObject.items.remove(item);
+                    });
+                }
+            };
+
+            self.textNode = $("<input type='text' data-sw-bind='value: itemToAdd, valueUpdate: keydown' class='_input_tagit' />");
+            self.textNode.keypress(function(e) {
+                if(e.which === 13) {
+                    var tag = tagsObject.itemToAdd();
+                    tagsObject.itemToAdd(""); // Clear text box
+                    if ((tag !== "") && (tagsObject.items.indexOf(tag) < 0)){
+                        // Prevent blanks and duplicates
+                        if (obj.filter && typeof obj.filter === 'function'){
+                            var ret = obj.filter(tag);
+                            if (!ret){ return; }
+                        }
+                        tagsObject.items.push(tag);
+                        if (obj.onTag && typeof obj.onTag === 'function'){
+                            obj.onTag(tag);
+                        }
+                    }
+                } else if (e.which === 8 && tagsObject.itemToAdd() === "") {
+                    var text = tagsObject.items.pop();
+                    tagsObject.itemToAdd(text);
+                }
+            });
+
+            //focus inside input on click
+            self.node.on("click", function(){
+                self.textNode.focus();
+            });
+
+            self.tagsNode = $("<ul class='_ul_tagit' data-sw-bind='foreach: items'>" +
+                               "<li class='_li_tagit'>" +
+                               "<span class='_text_tagit' data-sw-bind='text: $data'></span>" +
+                               "<span class='_icon_tagit' data-sw-bind='click: root.deleteTag'>X</span>" +
+                               "</li>" +
+                               "</ul>");
+
+            self.node.append(self.tagsNode);
+            self.node.append(self.textNode);
+
+            sw.renderElement(this.tagsNode, tagsObject);
+            sw.renderElement(this.textNode, tagsObject);
+        }
+    });
+})();
